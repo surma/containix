76717f4a0f884885b71f05999557b496a1ff2a74
Surma <surma@surma.dev>
2024-09-20 09:51:53 +0100

    Offer baselayer

---
+++ containix-base.nix
@@ -0,0 +1,17 @@
+{
+  iana-etc,
+  util-linux,
+  iproute2,
+  coreutils,
+  bash,
+  buildEnv,
+  lib,
+
+}@args:
+buildEnv {
+  name = "containix-base";
+  paths = lib.removeAttrs args [
+    "buildEnv"
+    "lib"
+  ];
+}
--- example/simple_container/flake.nix
+++ example/simple_container/flake.nix
@@ -3,40 +3,42 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/24.05";
     flake-utils.url = "github:numtide/flake-utils";
+    containix.url = "github:surma/containix/tmp";
   };
   outputs =
     {
       self,
       nixpkgs,
       flake-utils,
+      containix,
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        inherit (pkgs) mkShell buildEnv writeShellScriptBin;
+        inherit (pkgs)
+          mkShell
+          buildEnv
+          writeShellScriptBin
+          callPackage
+          ;
       in
       rec {
         packages.default = buildEnv {
           name = "simple-container";
-          paths =
-            [
-              (writeShellScriptBin "simple-container" ''
-                echo -e "\n# Environment"
-                env
-                echo -e "\n# Mounts"
-                mount
-                echo -e "\n# Network"
-                ip addr
-                echo -e "\n# ls ''${1:-/}"
-                ls -alh ''${1:-/}
-              '')
-            ]
-            ++ (with pkgs; [
-              coreutils
-              iproute2
-              util-linux
-            ]);
+          paths = [
+            (writeShellScriptBin "simple-container" ''
+              echo -e "\n# Environment"
+              env
+              # echo -e "\n# Mounts"
+              # mount
+              echo -e "\n# Network"
+              ip addr
+              echo -e "\n# ls ''${1:-/}"
+              ls -alh ''${1:-/}
+            '')
+            containix.base
+          ];
         };
         apps.default = flake-utils.lib.mkApp { drv = packages.default; };
         devShell = mkShell {
